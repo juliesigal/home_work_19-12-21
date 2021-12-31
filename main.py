fc9961da3c9bd8a9518898635e1ce7a7d60eb72c
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-12-31 21:37:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sys
-from datetime import datetime
 from Kita import Kita
-from sqlalchemy import text
 from db_config import local_session, create_all_entities
 from Students import Students
 from Db_repo import DbRepo
@@ -10,3 +7,19 @@ from Db_repo import DbRepo
 create_all_entities()
 
 repo = DbRepo(local_session)
+
+k1 = Kita(floor=2, num_of_student=15, class_avg=84.5)
+k2 = Kita(floor=3, num_of_student=18, class_avg=79.5)
+kita_ls = [k1, k2]
+#repo.add_all(kita_ls)
+
+st1 = Students(fname='Liron', lname='Cohen', grade_avg=83.9, kita_id=1)
+st2 = Students(fname='Daniel', lname='Liss', grade_avg=84, kita_id=2)
+st_list = [st1, st2]
+
+#repo.add_all(st_list)
+
+print(repo.get_by_condition(Students, lambda query: query.filter(Kita.id == 2).all()))
+print(repo.get_by_condition(Students, lambda query: query.filter(Kita.id == 1).all()))
+
+
